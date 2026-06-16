dc7bfbdaa48367abed2ec8a4f990613e9ef7c4f5
sunlight <87934951+heaveeenn@users.noreply.github.com>
2026-06-16 11:14:32 +0900

    Init project

---
+++ src/main/java/com/bitesave/BitesaveBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bitesave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitesaveBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BitesaveBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bitesave/BitesaveBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bitesave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitesaveBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
